35883a1f7d0f9b53b4bf9d4c41102caf66ad38bf
gsioutzos <sioutzos@yahoo.com>
2014-10-26 13:06:17 +0200

    no message

--- Assignment.java
+++ Assignment.java
@@ -1,7 +1,8 @@
 public class Assignment {
 	public static void FirstWork() {
-	 System.out.println( "I feel stupid with all this !" ); 
+	 // Prints I feel stupid with all this! on standard output.
+	 System.out.println( "I feel stupid with all this !" );
 	 }
 }
-	  
+
 
