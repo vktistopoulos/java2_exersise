1d43b3fac4edcf7632e9e45cff64f7255c9bf1aa
gsioutzos <sioutzos@yahoo.com>
2014-10-27 21:49:57 +0200

    Delete Assignment.java

--- Assignment.java
+++
@@ -1,8 +0,0 @@
-public class Assignment {
-	public static void FirstWork() {
-	 // Prints I feel stupid with all this! on standard output.
-	 System.out.println( "I feel stupid with all this !" );
-	 }
-}
-
-
