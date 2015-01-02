f13ec7770be4c8341c9f102d918c75b23e224545
G.S <sioutzos@yahoo.com>
2015-01-02 17:13:48 +0200

    Create Goldlike.java

---
+++ Goldlike.java
@@ -0,0 +1,8 @@
+public final class Goldlike  {
+public final int hayye;
+public Goldlike(int hayye) {
+if (hayye < 0)
+throw new IllegalArgumentException("IllegalArgumentException");
+this.hayye = hayye;
+}
+}
