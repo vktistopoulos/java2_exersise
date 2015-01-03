643b82e1c41ba1e5f197e13361d34d649b92ed10
BarousAnastasios <tbasos@hotmail.com>
2015-01-03 19:06:47 +0200

    Update Pinakas_suxnothtwn.java

--- Pinakas_suxnothtwn.java
+++ Pinakas_suxnothtwn.java
@@ -1,22 +1,121 @@
 import java.util.PriorityQueue;
 import java.util.Queue;
 
-/*Collects the frequencies of symbols in the stream that we want to compress and builds a code tree for it.*/
+/*Συλλογή των συχνοτήτων των συμβόλων που θέλουμε να συμπιέσουμε και χτίσιμο του κωδικού δέντρου για αυτό.*/
 
 public final class Pinakas_suxnothtwn {
 
 	private int[] frequencies;
-	public Pinakas_suxnothtwn(int[] freq) {
-		if (freq == null) {
-			throw new NullPointerException("Null Argument");
+	
+	public Pinakas_suxnothtwn(int[] freqs) {
+		if (freqs == null) {
+			throw new NullPointerException("Null");
 		}
-		if (freq.length < 2) {
-			throw new IllegalArgumentException("At least 2 symbols needed");
-			frequencies = freq.clone();
+		if (freqs.length < 2) {
+			throw new IllegalArgumentException("Τουλάχιστον 2 σύμβολα χρειάζονται");
+			frequencies = freqs.clone();
 		}
-		for (int x=0; x<=frequencies; x++) {
+		for (int x : frequencies) {
 			if (x < 0) {
-				throw new IllegalArgumentException("Negative frequency");
+				throw new IllegalArgumentException("Αρνητική συχνότητα");
 			}
 		}
 	}
+	
+	public int getSymbolLimit() {
+		return frequencies.length;
+	}
+
+	public int get(int symbol) {
+		if (symbol < 0 || symbol >= frequencies.length)
+			throw new IllegalArgumentException("Σύμβολο εκτός εύρους");
+		return frequencies[symbol];
+	}
+
+	public void set(int symbol, int freq) {
+		if (symbol < 0 || symbol >= frequencies.length)
+			throw new IllegalArgumentException("Σύμβολο εκτός εύρους");
+		frequencies[symbol] = freq;
+	}
+
+	public void aukshsh(int symbol) {
+		if (symbol < 0 || symbol >= frequencies.length)
+			throw new IllegalArgumentException("Σύμβολο εκτός εύρους");
+		if (frequencies[symbol] == Integer.MAX_VALUE)
+			throw new RuntimeException("Αριθμητική υπερχείλιση");
+		frequencies[symbol]++;
+	}
+	
+	// Επιστροφή συμβολοσειράς με όλα τα σύμβολα και τις συχνότητες.
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < frequencies.length; i++)
+			sb.append(String.format("%d\t%d%n", i, frequencies[i]));
+		return sb.toString();
+	}
+
+	// Επιστροφή ενός κώδικα δέντρου για αυτές τις συχνότητες.
+	public CodeTree xtisimoDentrouKwdika() {
+	
+		Queue<NodeWithFrequency> pqueue = new PriorityQueue<NodeWithFrequency>();
+
+		// Άθροισμα των φύλλων για τα σύμβολα με μη μηδενική συχνότητα.
+		for (int i = 0; i < frequencies.length; i++) {
+			if (frequencies[i] > 0)
+				pqueue.add(new NodeWithFrequency(new Leaf(i), i, frequencies[i]));
+		}
+
+		// Δημιουργία τουλάχιστον 2 στοιχείων.
+		for (int i = 0; i < frequencies.length && pqueue.size() < 2; i++) {
+			if (i >= frequencies.length || frequencies[i] == 0) {
+				pqueue.add(new NodeWithFrequency(new Leaf(i), i, 0));
+			}
+		}
+		if (pqueue.size() < 2) {
+			throw new AssertionError();
+		}
+
+		// Επαναληπτική ένωση των στοιχείων με τις χαμηλότερες συχνότητες.
+		while (pqueue.size() > 1) {
+			NodeWithFrequency nf1 = pqueue.remove();
+			NodeWithFrequency nf2 = pqueue.remove();
+			pqueue.add(new NodeWithFrequency(
+					new InternalNode(nf1.node, nf2.node),
+					Math.min(nf1.lowestSymbol, nf2.lowestSymbol),
+					nf1.frequency + nf2.frequency));
+		}
+
+		// Επιστροφή του υπολοίπου.
+		return new CodeTree((InternalNode)pqueue.remove().node, frequencies.length);
+	}
+
+	private static class NodeWithFrequency implements Comparable<NodeWithFrequency> {
+
+		public final Node node;
+
+		public final int lowestSymbol;
+
+		public final long frequency;
+
+		public NodeWithFrequency(Node node, int lowestSymbol, long freq) {
+			this.node = node;
+			this.lowestSymbol = lowestSymbol;
+			this.frequency = freq;
+		}
+
+		public int compareTo(NodeWithFrequency other) {
+			if (frequency < other.frequency) {
+				return -1;
+			} else if (frequency > other.frequency) {
+				return 1;
+			} else if (lowestSymbol < other.lowestSymbol) {
+				return -1;
+			} else if (lowestSymbol > other.lowestSymbol) {
+				return 1;
+			} else {
+				return 0;
+			}
+		}
+	}
+}
+
