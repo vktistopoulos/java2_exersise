ddb0c80a7287f58f749f32c5649a96a589064af8
vktistopoulos <t8130076@dias.aueb.gr>
2015-01-03 18:45:00 +0200

    Update and rename NumberOfBits.java to eisagwgh_bit.java

--- eisagwgh_bit.java
+++ eisagwgh_bit.java
@@ -1,24 +1,21 @@
-package nayuki.huffmancoding;
-
 import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStream;
 
-/**
-* A stream of bits that can be read. Because they come from an underlying byte stream, the total number of bits is always a multiple of 8. The bits are read in big endian.
-*/
 
-public final class NumberOfBits {
+
+
+public final class eisagwgh_bit {
       
-      // Underlying byte stream to read from.
+     
       private InputStream input;
-      // Either in the range 0x00 to 0xFF if bits are available, or is -1 if the end of stream is reached.
+    
       private int nextBits;
-      // Always between 0 and 7.
+     
       private int numBitsRemaining;
       private boolean isEndOfStream;
       
-      // Creates a bit input stream based on the given byte input stream.
+   
 
       public BitInputStream(InputStream in) {
             if (in == null)
@@ -28,7 +25,7 @@ public final class NumberOfBits {
             isEndOfStream = false;
       }
 
-      // Reads a bit from the stream. Returns 0 or 1 if a bit is available, or -1 if the end of stream is reached. The end of stream always occurs on a byte boundary.
+      
       
       public int read() throws IOException {
             if (isEndOfStream)
@@ -45,8 +42,7 @@ public final class NumberOfBits {
             return (nextBits >>> numBitsRemaining) & 1;
       }
 
-      // Reads a bit from the stream. Returns 0 or 1 if a bit is available, or throws an EOFException if the end of stream is reached.
-      
+    
       public int readNoEof() throws IOException {
             int result = read();
             if (result != -1)
@@ -55,7 +51,6 @@ public final class NumberOfBits {
                   throw new EOFException("End of stream reached");
       }
 
-      // Closes this stream and the underlying InputStream.
       
       public void close() throws IOException {
             input.close();
