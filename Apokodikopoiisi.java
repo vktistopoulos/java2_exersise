61d063213eb54c2759b63a3264f00163ec091db9
BarousAnastasios <tbasos@hotmail.com>
2015-01-09 00:15:50 +0200

    Update Apokodikopoiisi.java

--- Apokodikopoiisi.java
+++ Apokodikopoiisi.java
@@ -1,35 +1,37 @@
 public final class Apokodikopoiisi {
 
-	private eisagwgh_biτ input;
+	private eisagwgh_bit input;
 	//Prepei na arxikopoiithei prin kalesoume tin read();
 
 	public Kodikos_dentroy Kodikos_dentroy;
 
 	public Apokodikopoiisi(eisagwgh_bit  in) {
-			if (in == null)
-				throw new NullPointerException("Null argument");
+		if (in == null)
+			throw new NullPointerException("Null argument");
 			input = in;
 	}
 
 	public int read() throws IOException {
-			if (Kodikos_dentroy == null)
-				throw new NullPointerException("null code");
+		if (Kodikos_dentroy == null)
+			throw new NullPointerException("null code");
 
-			Esoterikos_komvos currentKomvos = Kodikos_dentroy.root;
-			while (true) {
-				int temp = input.readNoEof();
-				Komvos nextKomvos ;
-				if      (temp == 0) nextKomvos = currentKomvos.leftChild;
-				else if (temp == 1) nextKomvos = currenKomvos.rightChild;
-				else throw new AssertionError();
+		Esoterikos_komvos currentKomvos = Kodikos_dentroy.root;
+		while (true) {
+			int temp = input.readNoEof();
+			Komvos nextKomvos ;
+			if      (temp == 0) 
+				nextKomvos = currentKomvos.leftChild;
+			else if (temp == 1)
+				nextKomvos = currenKomvos.rightChild;
+			else throw new AssertionError();
 
-				if (nextKomvos instanceof  Fyllo)
-					return (( Fyllo)nextKomvos).symbol;
-				else if (nextKomvos instanceof Esoterikos_komvos)
-					currentKomvos = (Esoterikos_komvos)nextKomvos ;
-				else
-						throw new AssertionError();
-			}
+			if (nextKomvos instanceof  Fyllo)
+				return (( Fyllo)nextKomvos).symbol;
+			else if (nextKomvos instanceof Esoterikos_komvos)
+				currentKomvos = (Esoterikos_komvos)nextKomvos ;
+			else
+				throw new AssertionError();
+		}
 	}
 
 }
