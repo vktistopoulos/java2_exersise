c5a5c74c13f494c7c5b20a2d05d58b41d060b9f8
BarousAnastasios <tbasos@hotmail.com>
2015-01-09 00:26:00 +0200

    Update Kanonikos_kodikas.java

--- Kanonikos_kodikas.java
+++ Kanonikos_kodikas.java
@@ -3,82 +3,73 @@ import java.util.List;
 
 public final class Kanonikos_kodikas {
 
+	private int[] lengths;
 
-private int[] lengths;
+	public Kanonikos_kodikas(int[] lengths) {
+		if (lengths == null)
+			throw new NullPointerException("NullPointerException");
+		this.lengths = lengths.clone();
+		for (int y =0 ; x < lengths.length ; y++) {
+			if (y < 0)
+				throw new IllegalArgumentException("We can't accept this length");
+		}
+	}
 
-public Kanonikos_kodikas(int[] lengths) {
+	public Kanonikos_kodikas(Kodikos_dentroy tree, int symbolLim) {
+		lengths = new int[symbolLim];
+		buildCodeLengths(tree.root, 0);
+	}
 
-if (lengths == null)
-throw new NullPointerException("NullPointerException");
-this.lengths = lengths.clone();
-for (int y =0 ; x < lengths.length ; y++) {
-if (y < 0)
-throw new IllegalArgumentException("We can't accept this length");
-}
-}
+	private void buildCodeLengths(Komvos node, int vathos) {
+		if (Komvos instanceof Esoterikos_komvos) {
+			Esoterikos_komvos internalNode = (Esoterikos_komvos)node;
+			buildCodeLengths(Esoterikos_komvos.leftChild , depth + 1);
+			buildCodeLengths(Esoterikos_komvos.rightChild, depth + 1);
+		}else if (node instanceof Fyllo) {
+			int symbol = ((Fyllo)node).symbol;
+			if (lengths[symbol] != 0)
+					throw new AssertionError("Error");
+			if (symbol >= lengths.length)
+				throw new IllegalArgumentException("Error");
+				lengths[symbol] = vathos;
+			}else {
+				throw new AssertionError("We cant accept this type");
+		}
+	}
 
+	public int getSymbolLim() {
+		return lengths.length;
+	}
 
-public Kanonikos_kodikas(Kodikos_dentroy tree, int symbolLim) {
-lengths = new int[symbolLim];
-buildCodeLengths(tree.root, 0);
-}
+	public int getCodeLength(int symbol) {
+		if (symbol < 0 || symbol >= lengths.length)
+			throw new IllegalArgumentException("Symbol out of range");
+			return  lengths[symbol];
+	}
 
-private void buildCodeLengths(Komvos node, int vathos) {
-if (Komvos instanceof Esoterikos_komvos) {
-Esoterikos_komvos internalNode = (Esoterikos_komvos)node;
-buildCodeLengths(Esoterikos_komvos.leftChild , depth + 1);
-buildCodeLengths(Esoterikos_komvos.rightChild, depth + 1);
-} else if (node instanceof Fyllo) {
-int symbol = ((Fyllo)node).symbol;
-if (lengths[symbol] != 0)
-throw new AssertionError("Error");
-if (symbol >= lengths.length)
-throw new IllegalArgumentException("Error");
-lengths[symbol] = vathos;
-} else {
-throw new AssertionError("We cant accept this type");
-}
-}
+	public Kodikos_dentroy toKodikos_dentroy() {
+		List<Komvos> nodes = new ArrayList<Komvos>();
+		for (int u = max(lengths); u>= 1; u--) {
+			List<Komvos> newNodes = new ArrayList<Komvos>();
+			for (int j = 0; j < lengths.length; j++) {
+				if (lengths[j] == i)
+					newNodes.add(new  Fyllo(j));
+			}
+			for (int j = 0; j < nodes.size(); j += 2)
+				newNodes.add(new Esoterikos_komvos(nodes.get(j), nodes.get(j + 1)));
+				nodes = newNodes;
+				if (nodes.size() % 2 != 0)
+					throw new IllegalStateException("IllegalStateException");
+			}
+			if (nodes.size() != 2)
+				throw new IllegalStateException("IllegalStateException");
+			return new Kodikos_dentroy(new Esoterikos_komvos(nodes.get(0), nodes.get(1)), lengths.length);
+		}
 
-
-
-
-public int getSymbolLim() {
-
-return lengths.length;
-}
-public int getCodeLength(int symbol) {
-
-if (symbol < 0 || symbol >= lengths.length)
-throw new IllegalArgumentException("Symbol out of range");
-return  lengths[symbol];
-}
-
-public Kodikos_dentroy toKodikos_dentroy() {
-List<Komvos> nodes = new ArrayList<Komvos>();
-for (int u = max(lengths); u>= 1; u--) {
-List<Komvos> newNodes = new ArrayList<Komvos>();
-
-
-for (int j = 0; j < lengths.length; j++) {
-if (lengths[j] == i)
-newNodes.add(new  Fyllo(j));
-}
-
-for (int j = 0; j < nodes.size(); j += 2)
-newNodes.add(new Esoterikos_komvos(nodes.get(j), nodes.get(j + 1)));
-nodes = newNodes;
-if (nodes.size() % 2 != 0)
-throw new IllegalStateException("IllegalStateException");
-}
-if (nodes.size() != 2)
-throw new IllegalStateException("IllegalStateException");
-return new Kodikos_dentroy(new Esoterikos_komvos(nodes.get(0), nodes.get(1)), lengths.length);
-}
-private static int max(int[] array) {
-int apotelesma = array[0];
-for (int x : array)
-apotelesma = Math.max(x, result);
-return apotelesma;
-}
-}
+		private static int max(int[] array) {
+			int apotelesma = array[0];
+			for (int x : array) {
+				apotelesma = Math.max(x, result);
+				return apotelesma;
+			}
+		}
