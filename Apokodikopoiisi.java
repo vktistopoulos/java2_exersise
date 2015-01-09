b93fc94dbf132c97a337ea381c3d97c784f2d91f
vktistopoulos <t8130076@dias.aueb.gr>
2015-01-09 23:10:02 +0200

    Update Apokodikopoiisi.java

--- Apokodikopoiisi.java
+++ Apokodikopoiisi.java
@@ -1,40 +1,56 @@
-  import java.io.IOException;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
- public final class Apokodikopoiisi {
+// Αποσυμπιέζει ένα εισαγώμενο αρχείο ,το οποίο είχε συμπιεστεί με την κωδικοποίηση Χόφμαν, σε ένα εξαγώμενο αρχείο
+public final class Aposympiesi {
 
-	private Eisagwgh_bit input;
+	public static void main(String[] args) throws IOException {
+		if (args.length == 0) {
+			System.err.println("Usage: java HuffmanDecompress InputFile OutputFile");
+			System.exit(1);
+			return;
+		}
 
-	public Kodikos_dentroy kodikos_dentroy;
+		//Αλλιώς αποσυμπίεσε
+		File inputFile = new File(args[0]);
+		File outputFile = new File(args[1]);
 
-	public Apokodikopoiisi(Eisagwgh_bit in) {
-		if (in == null)
-			throw new NullPointerException("Null argument");
-			input = in;
-	}
-
-	public int read() throws IOException {
-		if (kodikos_dentroy == null)
-			throw new NullPointerException("null code");
-
-		Esoterikos_komvos currentKomvos = kodikos_dentroy.root;
-
-		while (true) {
-			int temp = input.readNoEof();
-			Komvos nextKomvos ;
-			if (temp == 0)
-				nextKomvos = currentKomvos.leftChild;
-			else if (temp == 1)
-				nextKomvos = currentKomvos.rightChild;
-			else throw new AssertionError();
-
-			if (nextKomvos instanceof  Fyllo)
-				return (( Fyllo)nextKomvos).symbol;
-			else if (nextKomvos instanceof Esoterikos_komvos)
-				currentKomvos = (Esoterikos_komvos)nextKomvos ;
-			else
-				throw new AssertionError();
+		Eisagwgh_bit in = new Eisagwgh_bit(new BufferedInputStream(new FileInputStream(inputFile)));
+		OutputStream out = new BufferedOutputStream(new FileOutputStream(outputFile));
+		try {
+			Kanonikos_kodikas canonCode = readCode(in);
+			Kodikos_dentroy codetree = canonCode.toCodeTree();
+			 aposympiesi(codetree, in, out);
+		} finally {
+			out.close();
+			in.close();
 		}
 	}
 
-}
+	static Kanonikos_kodikas readCode(Eisagwgh_bit in) throws IOException {
+		int[] lengths = new int[257];
+		for (int i = 0; i < lengths.length; i++) {
+			int val = 0;
+			for (int j = 0; j < 8; j++)
+				val = val << 1 | in.readNoEof();
+			lengths[i] = val ;
+		}
+		return new Kanonikos_kodikas(lengths);
+	}
 
+	static void aposympiesi(Kodikos_dentroy codetree, Eisagwgh_bit in, OutputStream out) throws IOException {
+                 Apokodikopoiisi ap = new Apokodikopoiisi(in);
+ 		ap.kodikos_dentroy = codetree ;
+		while (true) {
+			int symbol = ap.read();
+			if (symbol == 256)
+			break;
+			out.write(symbol);
+		}
+	}
+}
