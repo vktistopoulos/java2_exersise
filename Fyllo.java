217264b103244f63d8409d13bc5b3c88dc993ba0
vktistopoulos <t8130076@dias.aueb.gr>
2015-01-09 21:04:40 +0200

    Update Fyllo.java

--- Fyllo.java
+++ Fyllo.java
@@ -1,8 +1,8 @@
-public final class Fyllo  {
+public final class Fyllo extends Komvos {
   
   public final int symbol;
   
-  public Fyllo(int symbol) {
+  public Fyllo  int symbol {
     if (symbol < 0)
       throw new IllegalArgumentException("IllegalArgumentException");
       
